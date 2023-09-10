8076b41d7594bec23238c4db17149eb7bac2e868
Nícolas Amaral <nicolasmdoamaral@gmail.com>
2023-09-10 11:35:47 -0300

    First commit

--- source/scripts/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('example').addEventListener('click', function () {
-        document.getElementById('example2').style.display = 'none'
-    })
-})
